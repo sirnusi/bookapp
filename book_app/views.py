f633239233366385c035276f836d475cf1d0537d
sirnusi <davidsanusi360@gmail.com>
2021-09-29 06:26:58 +0100

    book commit

--- book_app/views.py
+++ book_app/views.py
@@ -10,22 +10,27 @@ from django.conf import settings
 from django.shortcuts import render
 # Create your views here.
 
+
 class HomeView(LoginRequiredMixin, ListView):
     model = Book
     template_name = 'book_app/home.html'
+    
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['books'] = Book.objects.filter(author=self.request.user)
         return context
 
+
 class Dashboard(LoginRequiredMixin, View):
     def get(self, *args, **kwargs):
         view = HomeView.as_view(template_name='book_app/dashboard.html')
         return view(*args, **kwargs)
     
+    
 class BookDetailView(LoginRequiredMixin, DetailView):
     model = Book
+    
     
     def get_object(self):
         object = super().get_object()
@@ -33,13 +38,16 @@ class BookDetailView(LoginRequiredMixin, DetailView):
         object.save()
         return object
     
+    
 class BookCreateView(LoginRequiredMixin, CreateView):
     model = Book
     form_class = BookForm
     
+    
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+        
         
 class BookUpdateView(LoginRequiredMixin, UpdateView):
     model = Book
@@ -50,36 +58,44 @@ class BookDeleteView(LoginRequiredMixin, DeleteView):
     model = Book
     success_url = reverse_lazy('dashboard')
 
+
 class BookGenre(ListView):
     template_name = 'book_app/book_by_genre.html'
     paginate_by = 2
     
+    
     def get_queryset(self):
         self.genre = get_object_or_404(Genre, name=self.kwargs['genre'])
         return Book.objects.filter(genre=self.genre, author=self.request.user)
+    
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['genre'] = self.genre
         return context
     
+    
 class CreateGenre(CreateView):
     model = Genre
     form_class = GenreForm
+
 
 class UserRegistration(CreateView):
     form_class = RegisterUser
     success_url = reverse_lazy('login')
     template_name = 'book_app/signup.html'
 
+
 class SearchResultsView(ListView):
     model = Book
     template_name = 'book_app/search_results.html'
+    
     
     def get_queryset(self):
         self.query = self.request.GET.get('q')
         object_list = Book.objects.filter(Q(title__contains=self.query))
         return object_list
+    
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -91,8 +107,10 @@ def sendMail(request):
     form = EmailForm()
     messageSent = False
     
+    
     if request.method == 'POST':
         form = EmailForm(request.POST)
+        
         
         if form.is_valid():
             cd = form.cleaned_data
@@ -102,6 +120,7 @@ def sendMail(request):
             messageSent = True
         else:
             form = EmailForm()
+            
             
     return render(request, 'book_app/send_email.html', {'form': form,'messageSent': messageSent})
         
